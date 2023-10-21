1411d424f2c5aeebf4f9d601ce722fef36542688
Petr Pokarzhevskiy <pokar.petr@gmail.com>
2023-10-21 22:01:03 +0200

    Modified bridge_files

--- wp-content/themes/bridge-montenegro-theme/includes/func/bridge_files.php
+++ wp-content/themes/bridge-montenegro-theme/includes/func/bridge_files.php
@@ -8,7 +8,7 @@ function bridge_files() {
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('custom-google-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:400i,700i|Roboto:400i,700i|Golos+Text:300,500,700');
 
-    wp_enqueue_script('google-map', '//maps.googleapis.com/maps/api/js?key=AIzaSyCFJUtQIV89Wq1V8g004V-4eHsFk90-2uY', NULL, '1.0', true);
+    wp_enqueue_script('google-map', '//maps.googleapis.com/maps/api/js?key=****', NULL, '1.0', true);
     wp_enqueue_script('main-bridge-script', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     // получаем начальный путь для Рест АПИ и одноразовый номер сессии
     wp_localize_script( 'main-bridge-script', 'bridge_data', array(
